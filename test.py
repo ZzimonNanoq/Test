a1bdb77653c7e6a171aca4d1718bb5c594348095
ZzimonNanoq <scalli15@student.aau.dk>
2018-05-19 17:22:21 +0200

    changed test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("test")
+import sys
+import subprocess
+import shlex
+
+command_line = input()
+args = shlex.split(command_line)
+print(command_line, 'john')
+#proc = subprocess.Popen('command', shell=False,stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+#proc.communicate(b'1')
